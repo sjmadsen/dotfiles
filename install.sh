141238c0c2d1ed0feaee5c698d262d16e0349061
Steve Madsen <steve.madsen@teamnorthwoods.com>
2019-11-22 11:29:56 -0500

    Install script for Bash and generic config files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+install -SC -m 0644 bashrc ~/.bashrc
+install -SC -m 0644 bash_profile ~/.bash_profile
+install -SC -m 0644 gemrc ~/.gemrc
+install -SC -m 0644 gitexcludes ~/.gitexcludes
+install -SC -m 0644 inputrc ~/.inputrc
+install -SC -m 0644 lldbinit ~/.lldbinit
+install -SC -m 0644 psqlrc ~/.psqlrc
