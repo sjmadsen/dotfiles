bed29b1898f49f29aa0df0a5c39fa963953eb5de
Steve Madsen <steve@lightyearsoftware.com>
2022-05-17 11:37:45 -0400

    Get PATH sorted out before rbenv initialization

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,5 +1,14 @@
 unsetopt autopushd
 
+if [ -d /opt/homebrew ]; then
+    export PATH="/opt/homebrew/bin:$PATH"
+fi
+
+export PATH="$HOME/bin:$PATH"
+export LESS=-eiMQRX
+export EDITOR="code -w"
+export VISUAL="$EDITOR"
+
 if whence -p rbenv >/dev/null; then
     eval "$(rbenv init -)"
     if whence -p brew >/dev/null; then
@@ -11,12 +20,3 @@ if [ -d "$HOME/.nvm" ]; then
     export NVM_DIR="$HOME/.nvm"
     source /usr/local/opt/nvm/nvm.sh
 fi
-
-if [ -d /opt/homebrew ]; then
-    export PATH="/opt/homebrew/bin:$PATH"
-fi
-
-export PATH="$HOME/bin:$PATH"
-export LESS=-eiMQRX
-export EDITOR="code -w"
-export VISUAL="$EDITOR"
